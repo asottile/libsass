f0631a23bd21d4af1c9d49eb88fa67149f24052f
Anthony Sottile <asottile@umich.edu>
2015-02-03 20:30:22 -0800

    ???

--- bld.bat
+++ bld.bat
@@ -1,3 +1,5 @@
-"C:/Program Files/Microsoft Visual Studio 12.0/VC/BIN/cl.exe" /c /nologo /Ox /MD /W3 /GS- /DNDEBUG *.cpp -c -O2 /Zi
-"C:/Program Files/Microsoft Visual Studio 12.0/VC/BIN/cl.exe" /c /nologo /Ox /MD /W3 /GS- /DNDEBUG *.c -c -O2 /Zi
-"C:\Program Files\Microsoft Visual Studio 12.0\VC\BIN\link.exe" /nologo /INCREMENTAL:NO *.obj /OUT:main.exe /Debug
+DIR
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat" x86
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\cl.exe" /c /nologo /Ox /MD /W3 /GS- /DNDEBUG *.c -c -O2 /Zi
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\cl.exe" /c /nologo /Ox /MD /W3 /GS- /DNDEBUG *.cpp -c -O2 /Zi
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\link.exe" /nologo /INCREMENTAL:NO *.obj /OUT:main.exe /Debug
