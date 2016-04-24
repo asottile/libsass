48d8fcc9751f9448942efa03f8722c4fa77091d9
Anthony Sottile <asottile@umich.edu>
2016-04-24 15:36:42 -0700

    Removing /GL fixes?

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 "D:\Programs\VS2015\VC\BIN\amd64\cl.exe" ^
     /I.\include -ID:\Programs\VS2015\VC\INCLUDE ^
-    /c /nologo /W3 /WX- /GL /DNDEBUG -O2 /Oi /Zi /EHsc /MT ^
+    /c /nologo /W3 /WX- /DNDEBUG -O2 /Oi /Zi /EHsc /MT ^
     src/*.c
 "D:\Programs\VS2015\VC\BIN\amd64\cl.exe" ^
     /I.\include ^
@@ -11,7 +11,7 @@
     "-IC:\Program Files (x86)\Windows Kits\8.1\include\\shared" ^
     "-IC:\Program Files (x86)\Windows Kits\8.1\include\\um" ^
     "-IC:\Program Files (x86)\Windows Kits\8.1\include\\winrt" ^
-    /c /nologo /W3 /WX- /GL /DNDEBUG -O2 /Oi /Zi /EHsc /MT ^
+    /c /nologo /W3 /WX- /DNDEBUG -O2 /Oi /Zi /EHsc /MT ^
     ./main.cpp src/*.cpp 
 "D:\Programs\VS2015\VC\BIN\amd64\link.exe" ^
     "-LIBPATH:D:\Programs\VS2015\VC\LIB\amd64" ^
@@ -19,6 +19,6 @@
     "-LIBPATH:C:\Program Files (x86)\Windows Kits\10\lib\10.0.10240.0\ucrt\x64" ^
     "-LIBPATH:C:\Program Files (x86)\Windows Kits\NETFXSDK\4.6.1\lib\um\x64" ^
     "-LIBPATH:C:\Program Files (x86)\Windows Kits\8.1\lib\winv6.3\um\x64" ^
-    "-INCREMENTAL:NO" "-OUT:main.exe" "-Debug" "-nologo" "-LTCG" ^
+    "-INCREMENTAL:NO" "-OUT:main.exe" "-Debug" "-nologo" ^
     *.obj
 main.exe
